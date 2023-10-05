21e1b8c1da8e2d5a16caf66a24004dbba6fbc23a
Vansales <>
2023-10-05 11:16:38 +0700

    no message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomRouter from './router/router'; // Adjust the path if needed
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <CustomRouter />
+    </React.StrictMode>
+  );
+};
+
+export default App;
